093ca246dbcaacf3c7161f10f07f441011ffd05e
Blair Harris <woolawin@gmail.com>
2026-03-07 13:56:15 +0100

    add openai

--- src/routes/api/translate/+server.ts
+++ src/routes/api/translate/+server.ts
@@ -1,10 +1,27 @@
+import OpenAI from "openai";
+import { OPENAI_API_KEY } from "$env/static/private";
+
 export async function POST({ request }) {
     const data = await request.json();
+
+    const client = new OpenAI({
+        apiKey: OPENAI_API_KEY,
+    });
+
+    const response = await client.responses.create({
+        model: "gpt-5",
+        input: [
+            { role: "user", content: "From the following Swedish text" },
+            { role: "user", content: data.original },
+            { role: "user", content: "translate the following into english " },
+            { role: "user", content: data.selected },
+        ],
+    });
 
     const outpayload = {
         original: data.original,
         selected: data.selected,
-        translation: "foobar",
+        translation: response.output_text,
     };
 
     return new Response(JSON.stringify(outpayload), {
